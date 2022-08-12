75f7aa9b614539292083e29497fa367ea8b5e975
hayakohe <kohei.hayashi@hrotti.co.jp>
2022-08-12 18:22:26 +0900

    update: HeroServiceをHeroesComponentにDI

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
-import { HEROES } from "../mock-heroes";
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -10,10 +10,10 @@ import { HEROES } from "../mock-heroes";
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
   }
