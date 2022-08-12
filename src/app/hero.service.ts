7d8e4a56068e28a0dcaa5b5e15533d31b0eb9272
hayakohe <kohei.hayashi@hrotti.co.jp>
2022-08-12 18:35:32 +0900

    update: MessageServiceをHeroServiceにDI

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './Hero';
+import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
 
 @Injectable({
@@ -8,7 +9,7 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
