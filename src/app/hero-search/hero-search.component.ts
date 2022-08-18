ec673b353a8389cf786548fa7b08976a5a63fd72
hayakohe <kohei.hayashi@hrotti.co.jp>
2022-08-18 18:11:14 +0900

    update: Hero検索機能を実装

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { debounceTime, distinctUntilChanged, switchMap, Observable, Subject, tap } from 'rxjs';
+import { Hero } from '../Hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-search',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-search.component.css']
 })
 export class HeroSearchComponent implements OnInit {
+  heroes$!: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300), // 300ms
+      distinctUntilChanged(),
+      tap(term => console.log(`${term}`)),
+      switchMap((term: string) => this.heroService.searchHeroes(term))
+    );
   }
 
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 }
