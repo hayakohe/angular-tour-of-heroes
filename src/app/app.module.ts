9c3b7d82c19d3db2ec41041d73a309d935d4771f
hayakohe <kohei.hayashi@hrotti.co.jp>
2022-08-18 17:43:57 +0900

    chore: hero-searchコンポーネントを生成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MessagesComponent } from './messages/messages.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { InMemoryDataService } from "./in-memory-data.service";
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { InMemoryDataService } from "./in-memory-data.service";
     DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
-    MessagesComponent
+    MessagesComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
