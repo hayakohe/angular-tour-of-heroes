ba68eb0fd615eaa8abcd1247f59cf928ef553eb9
hayakohe <kohei.hayashi@hrotti.co.jp>
2022-08-18 17:16:25 +0900

    update: 新しいHeroを追加する機能を実装

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,7 +19,16 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  getHeroes(): void {
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  private getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes)
   }
